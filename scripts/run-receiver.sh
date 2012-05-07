7878845713e3d38086fd7dcfd56cc75ca8d2160a
Trevor Norris <trev.norris@gmail.com>
2012-05-07 13:05:18 -0700

    small change to number of running processes

--- scripts/run-receiver.sh
+++ scripts/run-receiver.sh
@@ -3,7 +3,7 @@
 mkdir -p /tmp/socket-test
 chmod a+w /tmp/socket-test
 
-for i in {1..4}
+for i in {1..6}
 do
 	node receiver -p /tmp/socket-test/multiserv${i}.sock -f sockets/receiver${i}.sock &
 done
