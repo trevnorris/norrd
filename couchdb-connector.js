eee85e3402fbce69ea6e281f219e23c4406d1a87
Trevor Norris <trev.norris@gmail.com>
2012-05-07 13:03:05 -0700

    add debugging and cli options

--- couchdb-connector.js
+++ couchdb-connector.js
@@ -1,10 +1,25 @@
 var net = require( 'net' ),
-	nano = require( 'nano' )( 'http://localhost:5984' ),
-	db = nano.db.use( 'norrd-data' ),
-	netClient = net.connect( '/tmp/norrd-collector.sock' );
+	cli = require( 'commander' ),
+	nano = require( 'nano' ),
+	netClient, db;
+
+require( './utils' );
+
+cli.option( '-b, --db [name]', 'Name of CouchDB database to use', String, 'norrd-data' )
+	.option( '-c, --conn [url]', 'Location of the CouchDB instance', String, 'http://localhost' )
+	.option( '-p, --port [port]', 'Port number of the CouchDB instance', 5984 )
+	.option( '-n, --net [loc]', 'Location (URL or socket) of collector', '/tmp/norrd-collector.sock' )
+	.option( '-d, --debug', 'Enable debugging' )
+	.parse( process.argv );
+
+nano = nano( cli.conn + ':' + cli.port );
+db = nano.db.use( cli.db );
+netClient = net.connect( cli.net );
 
 function receiveDbData( er, ok ) {
-	if ( er ) console.log( 'ERROR:', er );
+	if ( cli.debug ) {
+		if ( er ) debugLog( 'ERROR:', er );
+	}
 }
 
 function onNetData( data ) {
