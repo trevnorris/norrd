3a4e5efa9cb2b4a0e4aaed4a8cbfd0479eadac7f
Trevor Norris <trev.norris@gmail.com>
2012-08-28 15:29:37 -0700

    Move server files to sub directory
    No need for unnecessary clutter. Moved the collector/receiver and
    associated files to server/.

--- scripts/run-receiver.sh
+++ scripts/run-receiver.sh
@@ -11,8 +11,8 @@ chmod a+w /tmp/norrd-receiver-out
 for i in {1..6}
 do
 	# use some cli args to run multiple instances
-	node $DIR/../receiver -p /tmp/norrd-receiver-in/multiserv${i}.sock -f /tmp/norrd-receiver-out/receiver${i}.sock &
+	node $DIR/../server/receiver -p /tmp/norrd-receiver-in/multiserv${i}.sock -f /tmp/norrd-receiver-out/receiver${i}.sock &
 done
 
 # run collector with defaults in config file
-node $DIR/../collector &
+node $DIR/../server/collector &
